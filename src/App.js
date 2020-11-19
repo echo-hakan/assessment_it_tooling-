4d2ef4c2ca157af85db9a551e0f06cf9f70be432
echo-hakan <hakanf_@hotmail.com>
2020-11-19 12:31:38 +0100

    bonus gerialiseerd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  var arr = ["b","o","n","u","s"];
   return (
     <div className="App">
       <header className="App-header">
         <h1>IT Tooling</h1>
+        <span>
+        { arr.map((value, index) => {
+          return <span> {value}</span>
+        })}
+        </span>
         <h3>de env code is: {process.env.REACT_APP_TEST}</h3>
       </header>
     </div>
