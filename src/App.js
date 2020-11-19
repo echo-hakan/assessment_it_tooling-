74bccb6a1f023464cabde75c41d406306ca0ed08
echo-hakan <hakanf_@hotmail.com>
2020-11-19 12:17:49 +0100

    env waarde wordt getoond

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>IT Tooling</h1>
-        <h3></h3> 
-        <input type="hidden" defaultValue={process.env.VAR_KEY_TEST} />
+        <h3>de env code is: {process.env.REACT_APP_TEST}</h3>
       </header>
     </div>
   );
